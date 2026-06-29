cc355efc4625d41151c55b5322b2d21a2d8af988
Anarasty <murloc.ninja16@gmail.com>
2026-06-29 20:59:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { Button, styled, Typography } from "@mui/material";
+import { Settings, Add } from "@mui/icons-material";
+
+const BlueBtn = styled(Button)({
+  backgroundColor: "skyblue",
+  color: "#888",
+  margin: 5,
+  "&:hover": {
+    backgroundColor: "lightblue",
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+    color: "white",
+  },
+});
+
+function App() {
+  return (
+    <>
+      Hello
+      <Button color="secondary" variant="text">
+        Text
+      </Button>
+      <Button variant="contained" color="success" startIcon={<Settings />}>
+        Settings
+      </Button>
+      <Button variant="outlined" startIcon={<Add />}>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        It is the p tag but has styles of a h1
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "lightblue",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+      >
+        Test
+      </Button>
+      <BlueBtn>A</BlueBtn>
+    </>
+  );
+}
+
+export default App;
